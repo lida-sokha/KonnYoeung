c67ef47e1c9ba002f43f66e6dae97ff098c31021
lida-sokha <sokhalida1234@gmail.com>
2026-01-17 16:52:05 +0700

    fix the homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from '../src/components/Layout/Navbar';
 import Footer from '../src/components/Layout/Footer';
+import HomePage from './pages/HomePage/HomePage';
 import Login  from './pages/Auth/Login';
 import SignUp from './pages/Auth/Signup';
-function App() {
+import AboutUsPage from './pages/Aboutus/About_us';
+import ContactUs from './pages/Contactus/Contact_us'; // Don't forget your new page!
 
 function App() {
   return (
     <Router>
-      <SignUp /> This stays on top of every page
+      {/* Navbar stays OUTSIDE Routes so it shows on every page */}
+      <Navbar /> 
+      
       <Routes>
-        <Route path="/" element={<AboutUsPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutUsPage />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
-      
-      
+
+      {/* Footer stays OUTSIDE Routes so it shows on every page */}
+      <Footer />
     </Router>
   );
 }
