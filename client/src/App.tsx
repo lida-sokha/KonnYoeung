52a9a85b784c532d5d385c6901c763c9586cf254
Sry kimsour <soursour3001@gmail.com>
2026-01-17 16:39:05 +0700

    updateaboutus

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from '../src/components/Layout/Navbar';
 import Footer from '../src/components/Layout/Footer';
-import Login  from './pages/Auth/Login';
-import SignUp from './pages/Auth/Signup';
-function App() {
-
+import AboutUsPage from './pages/Aboutus/About_us';
 function App() {
   return (
     <Router>
