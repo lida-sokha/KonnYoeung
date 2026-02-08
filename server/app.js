57a55cdae891949b7f28c02ff109c99b6cafc8ec
lida-sokha <sokhalida1234@gmail.com>
2026-02-08 23:26:24 +0700

    connect the backend and the frontend

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const setupSwagger = require("../server/src/config/swagger");
 const userRoutes = require("./src/routes/user.route.js");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
