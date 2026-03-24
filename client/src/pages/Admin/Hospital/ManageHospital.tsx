1aa70eee63443d7743f5930e8a2699778eb9687e
lida-sokha <sokhalida1234@gmail.com>
2026-03-24 17:41:33 +0700

    fixing the comfire error of the create user

--- client/src/pages/Admin/Hospital/ManageHospital.tsx
+++ client/src/pages/Admin/Hospital/ManageHospital.tsx
@@ -88,7 +88,7 @@ const handleDelete = async (hospitalId: string) => {
             <div className="p-6">
                 <div className="flex justify-between items-center mb-8">
                     <div>
-                        <h1 className="text-2xl font-bold text-gray-800">Manage Hospitals</h1>
+                        <h1 className="text-3xl font-bold text-gray-900">Manage Hospitals</h1>
                         <p className="text-gray-500 text-m">Add and manage hospital information</p>
                     </div>
                     <button
